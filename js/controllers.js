ac94d71d4aa3e2418b7efc7d0ac042b52d850843
jamasters <jamasters@agiledigital.com.au>
2013-06-26 21:53:01 +0930

    Exercise 8

--- js/controllers.js
+++ js/controllers.js
@@ -6,9 +6,25 @@ var geoScienceControllers = angular.module('geoScienceApp.controllers', []);
  * ViewOncCtrl is bound to the map display view that manages the retrieval of the site survey dataset and the
  * user requesting to add a new site survey.
  */
-geoScienceControllers.controller('ViewOncCtrl', ['$scope', 'SurveyService',
-function($scope, SurveyService) {
+geoScienceControllers.controller('ViewOncCtrl', ['$scope', '$location', 'SurveyService',
+function($scope, $location, SurveyService) {
+
+   // Retrieve existing markers from the persistent store.
    $scope.markers = SurveyService.getMarkers();
+   $scope.markerCount = $scope.markers.length;
+
+   // Function that will execute when the map is clicked, in order to add a pin to the map.
+   // This function is passed through to the mapping directive to customise functionality when the
+   // map is clicked.
+   $scope.clickedFunction = function(e) {
+      $location.url("/view2/" + e.latlng.lat + "/" + e.latlng.lng);
+   }
+   // Center the map somewhere near the center of Australia.
+   $scope.center = {
+      lat : -35.29207988159784,
+      lng : 149.12704467773438,
+      zoom : 14
+   };
 
 }]);
 
@@ -16,10 +32,15 @@ function($scope, SurveyService) {
  * View Two Controller is boud to the add site survey view and manages the upload of of a site image as well as recording the
  * site survey data to the persistent store.
  */
-geoScienceControllers.controller('ViewTwoCtrl', ['$scope',
-function($scope) {
+geoScienceControllers.controller('ViewTwoCtrl', ['$scope', '$location', '$routeParams', 'SurveyService',
+function($scope, $location, $routeParams, SurveyService) {
 
    $scope.siteSurvey = {};
+   
+   // Retrieve the lat and long from the route params. Probably not the best way to do this but is a good insight into
+   // the angular routing framework.
+   $scope.siteSurvey.lat = $routeParams.latitude;
+   $scope.siteSurvey.lng = $routeParams.longitude;
 
    // Handle the file upload result. Store the relative resource url for future
    // reference.
@@ -35,7 +56,14 @@ function($scope) {
       }
    };
 
+   /**
+    * Save a new marker record to the persistent store and redirect back to the mapping page.
+    */
    $scope.saveMarker = function() {
-      alert("Saving site survey with title [" + $scope.siteSurvey.title + "] and descrription [" + $scope.siteSurvey.description + "] and imageUrl [" + $scope.siteSurvey.imageUrl + "]");
+      SurveyService.saveMarker({}, $scope.siteSurvey, function(response) {
+         $location.url("/view1");
+      }, function(error) {
+         alert("Error")
+      });
    }
 }]);
--- js/directives.js
+++ js/directives.js
@@ -10,3 +10,14 @@ function(version) {
       elm.text(version);
    };
 }]);
+
+geoScienceDirectives.directive('fadeIn', function() {
+    return {
+      compile: function(elm) {
+        $(elm).css('opacity', 0.1);
+        return function(scope, elm, attrs) {
+          $(elm).animate({ opacity : 1.0 }, 1000 );
+        };
+      }
+    };
+  });
